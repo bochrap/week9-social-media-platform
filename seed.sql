c62299cebc95d94e900eb96f9b1d17addc8c2ad0
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-31 18:03:06 +0000

    add post functionality

--- seed.sql
+++ seed.sql
@@ -4,3 +4,10 @@ CREATE TABLE profiles(
     username TEXT,
     bio TEXT
 )
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    profile_id INTEGER REFERENCES profiles(id),
+    date TIMESTAMP
+)
--- src/app/user/[profileId]/addpost/page.jsx
+++ src/app/user/[profileId]/addpost/page.jsx
@@ -1,8 +1,22 @@
-export default function AddPost() {
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default function AddPost({ params }) {
+  async function handleAddPost(formData) {
+    "use server";
+
+    const content = formData.get("content");
+
+    await sql`INSERT INTO posts (content, profile_id, date) VALUES (${content}, ${params.profileId}, CURRENT_TIMESTAMP)`;
+    revalidatePath(`/user/${params.profileId}`);
+    redirect(`/user/${params.profileId}`);
+  }
+
   return (
-    <form action="" id="add-post-form">
+    <form action={handleAddPost} id="add-post-form">
       <h4>Add a new post</h4>
-      <textarea name="content" placeholder="What's on your mind?"></textarea>
+      <textarea name="content" placeholder="What's on your mind?" required></textarea>
       <button>Submit</button>
     </form>
   );
