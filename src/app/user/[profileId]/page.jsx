068a846a3cb4e8ae089591f9f83bf2f04ddf8a84
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-31 21:51:47 +0000

    add separator primitive

--- src/app/user/[profileId]/page.jsx
+++ src/app/user/[profileId]/page.jsx
@@ -1,8 +1,9 @@
 import { sql } from "@vercel/postgres";
 import { auth } from "@clerk/nextjs";
 import { notFound } from "next/navigation";
+import SeparatorDemo from "@/components/Separator";
 
-export default async function ProfilePage({ params }) {
+export default async function ProfilePage({ params, children }) {
   const { userId } = auth();
   const userData = await sql`SELECT * FROM profiles WHERE id = ${params.profileId}`;
 
@@ -15,8 +16,10 @@ export default async function ProfilePage({ params }) {
 
   return (
     <div>
-      <h3>`{userData.rows[0].username}&apos;s Profile Page`</h3>
-      <p>`Bio: {userData.rows[0].bio}`</p>
+      {/* <h3>`{userData.rows[0].username}&apos;s Profile Page`</h3>
+      <p>`Bio: {userData.rows[0].bio}`</p> */}
+      <SeparatorDemo username={userData.rows[0].username} bio={userData.rows[0].bio} params={params} />
+      <div> {children}</div>
     </div>
   );
 }
---
+++ src/components/Separator.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import * as Separator from "@radix-ui/react-separator";
+import "../app/globals.css";
+import Link from "next/link";
+
+const SeparatorDemo = ({ username, bio, params }) => (
+  <div style={{ width: "100%", maxWidth: 300, margin: "0 15px" }}>
+    <div className="Text" style={{ fontWeight: 500 }}>
+      {username}&apos;s Profile Page
+    </div>
+    <div className="Text">{bio}</div>
+    <Separator.Root className="SeparatorRoot" style={{ margin: "15px 0" }} />
+    <div style={{ display: "flex", height: 20, alignItems: "center" }}>
+      <div className="Text">
+        <Link href={`/user/${params.profileId}/posts`}>Posts</Link>
+      </div>
+      <Separator.Root className="SeparatorRoot" decorative orientation="vertical" style={{ margin: "0 15px" }} />
+      <div className="Text">Follows</div>
+      <Separator.Root className="SeparatorRoot" decorative orientation="vertical" style={{ margin: "0 15px" }} />
+      <div className="Text">Followed by</div>
+    </div>
+  </div>
+);
+
+export default SeparatorDemo;
