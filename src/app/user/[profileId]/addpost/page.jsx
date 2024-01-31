fb81c8af20b651abfd0cb85de9c6685ab3498e5f
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-31 17:07:45 +0000

    addpost route and empty form

---
+++ src/app/user/[profileId]/addpost/page.jsx
@@ -0,0 +1,9 @@
+export default function AddPost() {
+  return (
+    <form action="" id="add-post-form">
+      <h4>Add a new post</h4>
+      <textarea name="content" placeholder="What's on your mind?"></textarea>
+      <button>Submit</button>
+    </form>
+  );
+}
