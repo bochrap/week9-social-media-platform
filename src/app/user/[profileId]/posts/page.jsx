204624d60aef66b3a58603708973b09a739f442f
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-31 19:23:20 +0000

    posts as links

--- src/app/user/[profileId]/posts/page.jsx
+++ src/app/user/[profileId]/posts/page.jsx
@@ -1,17 +1,21 @@
 import { sql } from "@vercel/postgres";
+import Link from "next/link";
 
 export default async function GetUserPosts({ params }) {
   const userPosts = await sql`SELECT * FROM posts WHERE profile_id = ${params.profileId}`;
+  console.log(params);
 
   return (
     <div>
       <h3>User posts</h3>
       {userPosts.rows.map((post) => (
-        <div key={post.date} id="post-container">
-          {console.log(post.date)}
-          <p>User posted on {new Date(post.date).toLocaleString()}</p>
-          <p>{post.content}</p>
-        </div>
+        <Link id="post-link" key={post.date} href={`/user/${params.profileId}/posts/${post.id}`}>
+          <div key={post.date} id="post-container">
+            {console.log(post.date)}
+            <p>User posted on {new Date(post.date).toLocaleString()}</p>
+            <p>{post.content}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
