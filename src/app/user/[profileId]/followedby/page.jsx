3bdd30f5bd8855e7a6492e0de0fe5ceff944e1ab
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-01 21:27:35 +0000

    display followers list

--- src/app/user/[profileId]/followedby/page.jsx
+++ src/app/user/[profileId]/followedby/page.jsx
@@ -1,7 +1,30 @@
-export default async function FollowedBy() {
+import { sql } from "@vercel/postgres";
+import Link from "next/link";
+// import { auth } from "@clerk/nextjs";
+
+export default async function Followedby({ params }) {
+  //   console.log(currentUserId.rows[0].id);
+  const userData = await sql`SELECT * FROM profiles WHERE id = ${params.profileId}`;
+
+  console.log(userData.rows[0].username);
+
+  //GET LIST OF CURRENT USERS FOLLOWS
+  //   const usersFollows = await sql`SELECT * FROM follows WHERE follower_id = ${params.profileId}`;
+  const usersFollowers = await sql`SELECT follows.follower_id, follows.followee_id, profiles.username
+FROM follows
+JOIN profiles ON follows.follower_id = profiles.id
+WHERE follows.followee_id = ${params.profileId};`;
+  console.log(usersFollowers);
+
   return (
     <div>
       <h3>This user is followed by:</h3>
+      {usersFollowers.rows.map((follow) => (
+        <Link key={follow.follower_id} href={`/user/${follow.follower_id}/posts`}>
+          <p>{follow.username}</p>
+          <br />
+        </Link>
+      ))}
     </div>
   );
 }
