b35aeebad283643b2d7c6951a9c2aaebc3bc5c56
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-01 21:07:17 +0000

    display the list withprofiles followed by user

--- src/app/user/[profileId]/following/page.jsx
+++ src/app/user/[profileId]/following/page.jsx
@@ -1,4 +1,5 @@
 import { sql } from "@vercel/postgres";
+import Link from "next/link";
 // import { auth } from "@clerk/nextjs";
 
 export default async function Following({ params }) {
@@ -8,14 +9,27 @@ export default async function Following({ params }) {
   //   const currentUserId = await sql`SELECT id FROM profiles WHERE clerk_user_id = ${userId}`;
 
   //   console.log(currentUserId.rows[0].id);
+  const userData = await sql`SELECT * FROM profiles WHERE id = ${params.profileId}`;
+
+  console.log(userData.rows[0].username);
 
   //GET LIST OF CURRENT USERS FOLLOWS
-  const usersFollows = await sql`SELECT * FROM follows WHERE follower_id = ${params.profileId}`;
+  //   const usersFollows = await sql`SELECT * FROM follows WHERE follower_id = ${params.profileId}`;
+  const usersFollows = await sql`SELECT follows.follower_id, follows.followee_id, profiles.username
+FROM follows
+JOIN profiles ON follows.followee_id = profiles.id
+WHERE follows.follower_id = ${params.profileId};`;
   console.log(usersFollows);
 
   return (
     <div>
       <h3>This user is following:</h3>
+      {usersFollows.rows.map((follow) => (
+        <Link key={follow.followee_id} href={`/user/${follow.followee_id}/posts`}>
+          <p>{follow.username}</p>
+          <br />
+        </Link>
+      ))}
     </div>
   );
 }
