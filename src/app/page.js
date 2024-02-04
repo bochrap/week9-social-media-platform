d67bfe68ddeb32193bb2ff25d226f25aad167e7a
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-04 18:22:42 +0000

    addpost only works for currently logged in user

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import AllPosts from "./allposts/page";
+import Feed from "./feed/page";
+
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div>
+      Welcome to first social media platform for Filips <br />
+      <Link href="/allposts">All posts</Link>
+      <Link href="/feed">Your feed</Link>
+    </div>
+  );
 }
--- src/app/user/[profileId]/addpost/page.jsx
+++ src/app/user/[profileId]/addpost/page.jsx
@@ -1,14 +1,18 @@
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
 export default function AddPost({ params }) {
   async function handleAddPost(formData) {
     "use server";
 
+    const { userId } = auth();
+    const currentUserId = await sql`SELECT id FROM profiles WHERE clerk_user_id = ${userId}`;
+
     const content = formData.get("content");
 
-    await sql`INSERT INTO posts (content, profile_id, date) VALUES (${content}, ${params.profileId}, CURRENT_TIMESTAMP)`;
+    await sql`INSERT INTO posts (content, profile_id, date) VALUES (${content}, ${currentUserId.rows[0].id}, CURRENT_TIMESTAMP)`;
     revalidatePath(`/user/${params.profileId}/posts`);
     redirect(`/user/${params.profileId}/posts`);
   }
