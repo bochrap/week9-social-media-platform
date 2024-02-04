ba42917812b14c332b2c1660f1f617d7e62bd5d2
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-04 18:40:09 +0000

    links on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
 import Link from "next/link";
-import AllPosts from "./allposts/page";
-import Feed from "./feed/page";
+import { auth } from "@clerk/nextjs";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+  const currentUserId = await sql`SELECT id FROM profiles WHERE clerk_user_id = ${userId}`;
+
   return (
-    <div>
-      Welcome to first social media platform for Filips <br />
-      <Link href="/allposts">All posts</Link>
-      <Link href="/feed">Your feed</Link>
+    <div id="home-content">
+      <p>Welcome to first social media platform for Filips</p>
+      <p>
+        Explore <Link href="/allposts">All posts</Link>
+      </p>
+      <p>
+        Say something yourself <Link href={`/user/${currentUserId.rows[0].id}/addpost`}>xx</Link>
+      </p>
+      <p>
+        Or visit your profile page <Link href={`/user/${currentUserId.rows[0].id}`}>xx</Link>
+      </p>
     </div>
   );
 }
